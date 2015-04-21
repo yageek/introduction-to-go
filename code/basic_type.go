49483b7a3d7a8ac4501f9321375ebcd64f4758ba
Yannick heinrich <yannick.heinrich@gmail.com>
2015-04-22 01:04:44 +0200

    Add basic code

---
+++ code/basic_type.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var n int = 300
+	fmt.Println("n:", n)
+
+	pi := 3.1414562
+	fmt.Println("Pi:", pi)
+
+	message := "Some message"
+	fmt.Println("Message:", message)
+
+	var a float32 = 91.5
+	fmt.Println("a:", a)
+}
